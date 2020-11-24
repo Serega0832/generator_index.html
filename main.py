d32ecec7f8540041e7fc190efcdb3f52267cc47e
djuli <demidovich0831@gmail.com>
2020-11-24 23:20:05 +0300

    обновил бутстрап до 4.5 и добавил файл эбаут

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime as dt
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Сгенерировали страницу, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-# проверка на последние изменения
+
 
 def generate_head(title):
     head = f'''<head>
@@ -20,7 +20,19 @@ def generate_body(header, paragraphs):
     body = '<h1>' + header + '</h1>'
     for p in paragraphs:
         body += f'<p>{p}</p>'
-    return '<body>' + body + '</body>'
+    template ='''
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="#">Features</a>
+    <a class="p-2 text-dark" href="#">Enterprise</a>
+    <a class="p-2 text-dark" href="#">Support</a>
+    <a class="p-2 text-dark" href="#">Pricing</a>
+  </nav>
+  <a class="btn btn-outline-primary" href="#">Sign up</a>
+</div>
+    '''
+    return '<body>' + template+ body + '</body>'
 
 
 def generate_page(head, body):
