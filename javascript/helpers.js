3a5269a6157e64d31fc2c4d87c5f692e3454abff
djuli <demidovich0831@gmail.com>
2020-11-25 19:28:22 +0300

    теперь работает js, поставил привязку js в конец

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -1,7 +1,14 @@
-url_adviec = 'https://api.adviceslip.com/advice'
-function klikalka(){
-    ыу
-    p = $("#klikalka_id")
-    p.html("Привет")
+url_adviec = "https://api.adviceslip.com/advice"
+
+$("#main-header").click(function (){
+
+    $.getJSON(url_adviec, function (data) {
+        advice = (data["slip"].advice);
+    });
+    set_hello(advice)
+});
+function  set_hello(msg) {
+    p = $("#second-paragraphs")
+    p.html(msg)
     p.css('color', 'red')
 }
